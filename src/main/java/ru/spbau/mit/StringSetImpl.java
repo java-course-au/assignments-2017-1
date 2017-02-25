99024f3b1cf89ddf9ce8f939eba017c0599abac9
KruchDmitriy <kruch.dmitriy@gmail.com>
2017-02-25 16:31:27 +0300

    fix01

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -7,53 +7,63 @@ public class StringSetImpl implements StringSet {
         root = new Node();
     }
 
+    private Node find(String element) {
+        Node curNode = root;
+        for (int i = 0; i < element.length(); i++) {
+            curNode = curNode.getNext(element.charAt(i));
+            if (curNode == null) {
+                return null;
+            }
+        }
+        return curNode;
+    }
+
     @Override
     public boolean add(String element) {
         if (contains(element)) {
             return false;
-        } else {
-            Node curNode = root;
-            for (int i = 0; i < element.length(); i++) {
-                curNode.increaseNumStrings();
-                if (curNode.getNext(element.charAt(i)) == null) {
-                    curNode = curNode.setNext(element.charAt(i));
-                } else {
-                    curNode = curNode.getNext(element.charAt(i));
-                }
-            }
-            curNode.increaseNumStrings();
-            curNode.setIsEndOfString(true);
-            return true;
         }
+
+        Node curNode = root;
+        for (int i = 0; i < element.length(); i++) {
+            curNode.increaseNumStrings();
+            char curChar = element.charAt(i);
+            if (curNode.getNext(curChar) == null) {
+                curNode = curNode.setNext(curChar);
+            } else {
+                curNode = curNode.getNext(curChar);
+            }
+        }
+
+        curNode.increaseNumStrings();
+        curNode.setIsEndOfString(true);
+        return true;
     }
 
     @Override
     public boolean contains(String element) {
-        Node curNode = root;
-        for (int i = 0; i < element.length(); i++) {
-            if (curNode.getNext(element.charAt(i)) == null) {
-                return false;
-            } else {
-                curNode = curNode.getNext(element.charAt(i));
-            }
-        }
-        return curNode.isEndOfString();
+        Node node = find(element);
+        return node != null && node.isEndOfString();
     }
 
     @Override
     public boolean remove(String element) {
         if (!contains(element)) {
             return false;
-        } else {
-            Node curNode = root;
-            for (int i = 0; i < element.length(); i++) {
-                curNode.decreaseNumStrings();
-                curNode = curNode.getNext(element.charAt(i));
-            }
-            curNode.decreaseNumStrings();
-            curNode.setIsEndOfString(false);
-            return true;
         }
+        Node curNode = root;
+        for (int i = 0; i < element.length(); i++) {
+            curNode.decreaseNumStrings();
+            Node prevNode = curNode;
+            char curChar = element.charAt(i);
+            curNode = curNode.getNext(curChar);
+            if (curNode.getNumStrings() == 0) {
+                prevNode.delete(curChar);
+            }
+        }
+        curNode.decreaseNumStrings();
+        curNode.setIsEndOfString(false);
+        return true;
     }
 
     @Override
@@ -63,69 +73,60 @@ public class StringSetImpl implements StringSet {
 
     @Override
     public int howManyStartsWithPrefix(String prefix) {
-        Node curNode = root;
-        for (int i = 0; i < prefix.length(); i++) {
-            if (curNode.getNext(prefix.charAt(i)) == null) {
-                return 0;
+        Node node = find(prefix);
+        return node == null ? 0 : node.getNumStrings();
+    }
+
+    private static class Node {
+        private static final int NUMBER_OF_LETTERS = 52;
+        private Node[] nodes = new Node[NUMBER_OF_LETTERS];
+        private boolean isEndOfString = false;
+        private int numStrings = 0;
+
+        private static int getIndexFromChar(char chr) {
+            int ichr = (int) chr;
+            final int a = 65;
+            final int z = 90;
+            final int bigA = 97;
+            if (a <= ichr && ichr <= z) {
+                return ichr - a;
+            } else { // (A <= ichr <= Z)
+                return ichr - bigA + NUMBER_OF_LETTERS / 2;
             }
-            curNode = curNode.getNext(prefix.charAt(i));
         }
 
-        return curNode.getNumStrings();
-    }
-}
-
-class Node {
-    private static final int NUMBER_OF_LETTERS = 52;
-    private Node[] nodes;
-    private boolean isEndOfString;
-    private int numStrings;
-
-    Node() {
-        nodes = new Node[NUMBER_OF_LETTERS];
-        isEndOfString = false;
-        numStrings = 0;
-    }
-
-    private int idxFromChar(char chr) {
-        int ichr = (int) chr;
-        final int a = 65;
-        final int z = 90;
-        final int bigA = 97;
-        if (a <= ichr && ichr <= z) {
-            return ichr - a;
-        } else { // (A <= ichr <= Z)
-            return ichr - bigA + NUMBER_OF_LETTERS / 2;
+        Node getNext(char chr) {
+            return nodes[getIndexFromChar(chr)];
         }
-    }
 
-    Node getNext(char chr) {
-        return nodes[idxFromChar(chr)];
-    }
+        Node setNext(char chr) {
+            Node newNode = new Node();
+            nodes[getIndexFromChar(chr)] = newNode;
+            return newNode;
+        }
 
-    Node setNext(char chr) {
-        Node newNode = new Node();
-        nodes[idxFromChar(chr)] = newNode;
-        return newNode;
-    }
+        void delete(char chr) {
+            nodes[getIndexFromChar(chr)] = null;
+        }
 
-    boolean isEndOfString() {
-        return isEndOfString;
-    }
+        boolean isEndOfString() {
+            return isEndOfString;
+        }
 
-    void setIsEndOfString(boolean value) {
-        isEndOfString = value;
-    }
+        void setIsEndOfString(boolean value) {
+            isEndOfString = value;
+        }
 
-    int getNumStrings() {
-        return numStrings;
-    }
+        int getNumStrings() {
+            return numStrings;
+        }
 
-    void increaseNumStrings() {
-        numStrings++;
-    }
+        void increaseNumStrings() {
+            numStrings++;
+        }
 
-    void decreaseNumStrings() {
-        numStrings--;
+        void decreaseNumStrings() {
+            numStrings--;
+        }
     }
 }
