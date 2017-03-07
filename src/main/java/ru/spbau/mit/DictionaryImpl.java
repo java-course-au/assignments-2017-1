2a0e12d01b22d7b1751e84ef56643814f0546621
KruchDmitriy <kruch.dmitriy@gmail.com>
2017-03-07 13:21:57 +0300

    added implementation

---
+++ src/main/java/ru/spbau/mit/DictionaryImpl.java
@@ -0,0 +1,160 @@
+package ru.spbau.mit;
+
+public class DictionaryImpl implements Dictionary {
+    private static final int START_NUM_BUCKETS = 47;
+    private static final int BUCKET_SIZE = 10;
+    private int numBuckets = START_NUM_BUCKETS;
+    private ArrayDict[] buckets = new ArrayDict[numBuckets];
+    private int size = 0;
+
+    {
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayDict();
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean contains(String key) {
+        ArrayDict bucket = buckets[key.hashCode() % numBuckets];
+        return bucket.find(key) != null;
+    }
+
+    @Override
+    public String get(String key) {
+        ArrayDict bucket = buckets[key.hashCode() % numBuckets];
+        return bucket.find(key);
+    }
+
+    @Override
+    public String put(String key, String value) {
+        ArrayDict bucket = buckets[key.hashCode() % numBuckets];
+        String oldValue = bucket.replace(key, value);
+        if (oldValue == null) {
+            size++;
+
+            if (bucket.getSize() == BUCKET_SIZE) {
+                rehash();
+            }
+        }
+
+        return oldValue;
+    }
+
+    private void rehash() {
+        int newNumBuckets = numBuckets * 2;
+        ArrayDict[] newBuckets = new ArrayDict[newNumBuckets];
+        for (int i = 0; i < newNumBuckets; i++) {
+            newBuckets[i] = new ArrayDict();
+        }
+
+        for (int i = 0; i < numBuckets; i++) {
+            ArrayDict bucket = buckets[i];
+            for (int j = 0; j < bucket.getSize(); j++) {
+                String key = bucket.getKey(j);
+                String value = bucket.getValue(j);
+                ArrayDict newBucket = newBuckets[key.hashCode() % newNumBuckets];
+                newBucket.replace(key, value);
+            }
+        }
+
+        numBuckets = newNumBuckets;
+        buckets = newBuckets;
+    }
+
+    @Override
+    public String remove(String key) {
+        ArrayDict bucket = buckets[key.hashCode() % numBuckets];
+        String oldValue = bucket.remove(key);
+        if (oldValue != null) {
+            size--;
+        }
+        return oldValue;
+    }
+
+    @Override
+    public void clear() {
+        for (ArrayDict bucket : buckets) {
+            bucket.clear();
+        }
+    }
+
+    private class ArrayDict {
+        private int size = 0;
+        private String[] keys;
+        private String[] values;
+
+        ArrayDict() {
+            size = 0;
+            keys = new String[BUCKET_SIZE];
+            values = new String[BUCKET_SIZE];
+        }
+
+        int getSize() {
+            return size;
+        }
+
+        String getKey(int i) {
+            return keys[i];
+        }
+
+        String getValue(int i) {
+            return values[i];
+        }
+
+        private void add(String key, String value) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
+
+        String replace(String key, String value) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i].equals(key)) {
+                    String oldValue = values[i];
+                    values[i] = value;
+                    return oldValue;
+                }
+            }
+
+            add(key, value);
+            return null;
+        }
+
+        String find(String key) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i].equals(key)) {
+                    return values[i];
+                }
+            }
+            return null;
+        }
+
+        String remove(String key) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i].equals(key)) {
+                    keys[i] = keys[size - 1];
+                    keys[size - 1] = null;
+
+                    final String value = values[i];
+                    values[i] = values[size - 1];
+                    values[size - 1] = null;
+
+                    size--;
+                    return value;
+                }
+            }
+            return null;
+        }
+
+        void clear() {
+            size = 0;
+            keys = new String[BUCKET_SIZE];
+            values = new String[BUCKET_SIZE];
+        }
+    }
+}
--- src/test/java/ru/spbau/mit/DictionaryTest.java
+++ src/test/java/ru/spbau/mit/DictionaryTest.java
@@ -2,6 +2,8 @@ package ru.spbau.mit;
 
 import org.junit.Test;
 
+import java.util.UUID;
+
 import static org.junit.Assert.*;
 
 public class DictionaryTest {
@@ -9,6 +11,57 @@ public class DictionaryTest {
     public void testSimplePutGet() {
         Dictionary dict = instance();
         assertNull(dict.get("abc"));
+
+        assertNull(dict.put("abc", "cde"));
+        assertEquals("cde", dict.get("abc"));
+    }
+
+    @Test
+    public void crashTest() {
+        Dictionary dict = instance();
+        assertNull(dict.get("abc"));
+
+        String[] keys = new String[500];
+        String[] values = new String[500];
+        for (int i = 0; i < 500; i++) {
+            String key = String.valueOf(i);
+            String value = UUID.randomUUID().toString();
+            keys[i] = key;
+            values[i] = value;
+            assertEquals(dict.size(), i);
+            assertNull(dict.put(key, value));
+        }
+
+        for (int i = 0; i < 500; i++) {
+            assertEquals(values[i], dict.get(keys[i]));
+        }
+
+        for (int i = 0; i < 500; i++) {
+            dict.remove(keys[i]);
+            assertFalse(dict.contains(keys[i]));
+            assertEquals(499 - i, dict.size());
+        }
+
+        assertNull(dict.put("abc", "cde"));
+        assertEquals("cde", dict.get("abc"));
+    }
+
+    @Test
+    public void testClear() {
+        Dictionary dict = instance();
+        assertNull(dict.get("abc"));
+
+        for (int i = 0; i < 500; i++) {
+            String key = String.valueOf(i);
+            assertNull(dict.put(key, key));
+        }
+
+        dict.clear();
+
+        for (int i = 0; i < 500; i++) {
+            assertFalse(dict.contains(String.valueOf(i)));
+            assertNull(dict.get(String.valueOf(i)));
+        }
 
         assertNull(dict.put("abc", "cde"));
         assertEquals("cde", dict.get("abc"));
@@ -27,3 +80,4 @@ public class DictionaryTest {
         throw new IllegalStateException("Error while class loading");
     }
 }
+
