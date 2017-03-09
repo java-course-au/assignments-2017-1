85f595836698047fddedb25dccc71fe30cb90c2b
bugogish <bugogish@gmail.com>
2017-03-09 22:30:34 +0300

    dictionary implemented

---
+++ src/main/java/ru/spbau/mit/DictionaryImpl.java
@@ -0,0 +1,221 @@
+package ru.spbau.mit;
+
+public class DictionaryImpl implements Dictionary {
+    private static final class DictionaryBucket {
+        private static final class DictionaryNode {
+            private String key;
+            private String value;
+            private DictionaryNode next;
+            private DictionaryNode prev;
+
+            private DictionaryNode(String key, String value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private DictionaryNode head;
+        private int size = 1;
+
+        private DictionaryBucket(String key, String value) {
+            head = new DictionaryNode(key, value);
+        }
+
+        private DictionaryNode find(String key) {
+            DictionaryNode iter = head;
+
+            while (iter != null && !iter.key.equals(key)) {
+                iter = iter.next;
+            }
+            return iter;
+        }
+
+        private boolean contains(String key) {
+            return find(key) != null;
+        }
+
+        private String put(String key, String value) {
+            DictionaryNode node = find(key);
+
+            if (node == null) {
+                DictionaryNode newNode = new DictionaryNode(key, value);
+                newNode.next = head;
+                if (head != null) {
+                    head.prev = newNode;
+                }
+                head = newNode;
+                size++;
+                return null;
+            } else {
+                String result = node.value;
+                node.value = value;
+                return result;
+            }
+        }
+
+        private String get(String key) {
+            DictionaryNode node = find(key);
+            return node == null ? null : node.value;
+        }
+
+        private String remove(String key) {
+            DictionaryNode node = find(key);
+
+            if (node == null) {
+                return null;
+            } else {
+                String result = node.value;
+                if (node == head) {
+                    head = node.next;
+                } else {
+                    node.prev.next = node.next;
+                    if (node.next != null) {
+                        node.next.prev = node.prev;
+                    }
+                }
+                size--;
+                return result;
+            }
+        }
+
+        private String[] getKeys() {
+            String[] keys = new String[size];
+            DictionaryNode iter = head;
+
+            for (int i = 0; iter != null; i++) {
+                keys[i] = iter.key;
+                iter = iter.next;
+            }
+
+            return keys;
+        }
+    }
+
+    private static final float LOAD_FACTOR_RATE = 4 / 3;
+    private static final int INITIAL_CAPACITY = 11;
+
+    private int capacity = INITIAL_CAPACITY;
+    private int size = 0;
+    private DictionaryBucket[] hashMap = new DictionaryBucket[capacity];
+
+    private int getBucketIndex(String key) {
+        return Math.abs(key.hashCode()) % capacity;
+    }
+
+    private DictionaryBucket getBucket(String key) {
+        int index = getBucketIndex(key);
+        return hashMap[index];
+    }
+
+    private float maxLoadFactor() {
+        return LOAD_FACTOR_RATE * capacity;
+    }
+
+    private void rehash() {
+        capacity *= 2;
+        DictionaryBucket[] oldHashMap = hashMap;
+        hashMap = new DictionaryBucket[capacity];
+        size = 0;
+
+        for (DictionaryBucket bucket : oldHashMap) {
+            if (bucket != null) {
+                String[] keys = bucket.getKeys();
+                for (String key : keys) {
+                    put(key, bucket.get(key));
+                }
+            }
+        }
+    }
+
+    /**
+     * @return the number of keys
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * @param key
+     * @return true iff the dictionary contains `key`
+     */
+    @Override
+    public boolean contains(String key) {
+        DictionaryBucket bucket = getBucket(key);
+        return bucket != null && bucket.contains(key);
+    }
+
+    /**
+     * @param key
+     * @return mapping from the given key to it's value, or null, if the dictionary
+     * does not contain the key
+     */
+    @Override
+    public String get(String key) {
+        DictionaryBucket bucket = getBucket(key);
+        return bucket == null ? null : bucket.get(key);
+    }
+
+    /**
+     * Put new key-value pair into the dictionary,
+     * or update the value for the given key if it already exists
+     *
+     * @param key
+     * @param value
+     * @return if the dictionary already contains the key, return an old value,
+     * otherwise return null
+     */
+    @Override
+    public String put(String key, String value) {
+        if (size >= maxLoadFactor()) {
+            rehash();
+        }
+
+        int index = getBucketIndex(key);
+        DictionaryBucket bucket = hashMap[index];
+
+        if (bucket == null) {
+            hashMap[index] = new DictionaryBucket(key, value);
+            size++;
+            return null;
+        } else {
+            String result = bucket.put(key, value);
+            if (result == null) {
+                size++;
+            }
+            return result;
+        }
+    }
+
+    /**
+     * Remove the key-value from the dictionary if it exists there
+     *
+     * @param key
+     * @return an associated value to the key or null if the dictionary
+     * doesn't contain the key
+     */
+    @Override
+    public String remove(String key) {
+        DictionaryBucket bucket = getBucket(key);
+
+        if (bucket == null) {
+            return null;
+        } else {
+            String result = bucket.remove(key);
+            if (result != null) {
+                size--;
+            }
+            return result;
+        }
+    }
+
+    /**
+     * Remove all key-value pairs from the dictionary
+     */
+    @Override
+    public void clear() {
+        capacity = INITIAL_CAPACITY;
+        hashMap = new DictionaryBucket[capacity];
+        size = 0;
+    }
+}
--- src/test/java/ru/spbau/mit/DictionaryTest.java
+++ src/test/java/ru/spbau/mit/DictionaryTest.java
@@ -14,6 +14,86 @@ public class DictionaryTest {
         assertEquals("cde", dict.get("abc"));
     }
 
+    @Test
+    public void testPutAndClear() {
+        Dictionary dict = instance();
+        final int dictFinalSize = 30000;
+
+        for (int i = 0; i < dictFinalSize; i++) {
+            assertNull(dict.put(Integer.toString(i), null));
+        }
+
+        assertEquals(dictFinalSize, dict.size());
+        dict.clear();
+        assertEquals(0, dict.size());
+        assertNull(dict.get(Integer.toString(dictFinalSize - 1)));
+    }
+
+    @Test
+    public void testContains() {
+        Dictionary dict = instance();
+        String testSting = "test";
+
+        assertFalse(dict.contains(testSting));
+        assertNull(dict.put(testSting, testSting));
+        assertTrue(dict.contains(testSting));
+    }
+
+    @Test
+    public void testRemove() {
+        Dictionary dict = instance();
+        String testString = "test";
+
+        assertNull(dict.remove(testString));
+        assertNull(dict.put(testString, testString));
+        assertTrue(dict.contains(testString));
+        assertEquals(testString, dict.remove(testString));
+        assertFalse(dict.contains(testString));
+        assertEquals(0, dict.size());
+    }
+
+    @Test
+    public void testPutAndRemove() {
+        Dictionary dict = instance();
+        final int testsNum = 30000;
+
+        for (int i = 0; i < testsNum; i++) {
+            assertNull(dict.put(Integer.toString(i), Integer.toString(i)));
+            assertEquals(i + 1, dict.size());
+            assertEquals(Integer.toString(i), dict.get(Integer.toString(i)));
+        }
+
+        int size = testsNum;
+
+        for (int i = 0; i < testsNum; i += 2) {
+            assertEquals(Integer.toString(i), dict.remove(Integer.toString(i)));
+            assertNull(dict.remove(Integer.toString(i)));
+            size--;
+            assertEquals(size, dict.size());
+        }
+
+        assertEquals(testsNum / 2, dict.size());
+    }
+
+    @Test
+    public void testAddJustRemoved() {
+        Dictionary dict = instance();
+
+        assertNull(dict.put("Test String Key", "Test String Value"));
+        assertEquals("Test String Value", dict.remove("Test String Key"));
+        assertNull(dict.put("Test String Key", "Test String Value"));
+    }
+
+    @Test
+    public void testUpdateValue() {
+        Dictionary dict = instance();
+
+        assertNull(dict.put("first", "first value"));
+        assertEquals("first value", dict.put("first", "second value"));
+        assertEquals("second value", dict.get("first"));
+        assertEquals(1, dict.size());
+    }
+
     private static Dictionary instance() {
         try {
             return (Dictionary) Class.forName("ru.spbau.mit.DictionaryImpl").newInstance();
