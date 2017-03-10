48506f09ee6cd724bcddfde0ac720c82aaaaa20c
Anton Garder <avgarder@gmail.com>
2017-03-10 11:43:12 +0300

    add serialization to StringSetImpl

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -1,8 +1,10 @@
 package ru.spbau.mit;
 
+import java.io.*;
+import java.util.StringTokenizer;
 import java.util.function.Function;
 
-public class StringSetImpl implements StringSet {
+public class StringSetImpl implements StringSet, StreamSerializable {
     private static final int ALPHABET_SIZE = 52;
 
     private int size = 0;
@@ -50,6 +52,103 @@ public class StringSetImpl implements StringSet {
             return 0;
         }
         return lastNode.terminalsNumber;
+    }
+
+    @Override
+    public void serialize(OutputStream out) {
+        PrintWriter pw = new PrintWriter(out);
+        pw.println(size);
+        traverse(root, pw);
+        pw.close();
+        if (pw.checkError()) {
+            throw new SerializationException();
+        }
+    }
+
+    private void traverse(Node n, PrintWriter pw) {
+        pw.print(n.isTerminal ? 1 : 0);
+        pw.print(" ");
+        pw.println(n.terminalsNumber);
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            if (n.children[i] != null) {
+                pw.print(i);
+                pw.print(" ");
+            }
+        }
+        pw.println();
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            if (n.children[i] != null) {
+                traverse(n.children[i], pw);
+            }
+        }
+    }
+
+    @Override
+    public void deserialize(InputStream in) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        size = safeParseInt(safeRead(reader));
+        root = new Node();
+        restore(root, reader);
+    }
+
+    private void restore(Node cur, BufferedReader br) {
+        StringTokenizer tokens = new StringTokenizer(safeRead(br));
+        switch (safeNextToken(tokens)) {
+            case "1":
+                cur.isTerminal = true;
+                break;
+            case "0":
+                cur.isTerminal = false;
+                break;
+            default:
+                throw new SerializationException();
+        }
+        cur.terminalsNumber = safeParseInt(safeNextToken(tokens));
+
+        tokens = new StringTokenizer(safeRead(br));
+        while (tokens.hasMoreTokens()) {
+            int idx = safeParseInt(tokens.nextToken());
+            if (idx < 0 || idx >= ALPHABET_SIZE) {
+                throw new SerializationException();
+            }
+            cur.children[idx] = new Node();
+        }
+
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            if (cur.children[i] != null) {
+                restore(cur.children[i], br);
+            }
+        }
+    }
+
+    private String safeRead(BufferedReader br) {
+        try {
+            String s = br.readLine();
+            if (s == null) {
+                throw new SerializationException();
+            }
+            return s;
+        } catch (IOException e) {
+            throw new SerializationException();
+        }
+    }
+
+    private String safeNextToken(StringTokenizer tokenizer) {
+        if (!tokenizer.hasMoreTokens()) {
+            throw new SerializationException();
+        }
+        return tokenizer.nextToken();
+    }
+
+    private int safeParseInt(String s) {
+        if (s == null) {
+            throw new SerializationException();
+        }
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new SerializationException();
+        }
     }
 
     private Node findNode(String s) {
