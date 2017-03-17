4a2203e09d4359e85c728e056dd2f32335e95c71
YorovSobir <sobir.yorov94@gmail.com>
2017-03-17 22:27:30 +0300

    Log4j

---
+++ src/main/java/ru/spbau/mit/LogTest.java
@@ -0,0 +1,13 @@
+package ru.spbau.mit;
+
+import org.apache.logging.log4j.*;
+import java.lang.Double;
+import java.lang.Integer;
+
+public class LogTest {
+    private static final Logger LOGGER = LogManager.getLogger(LogTest.class);
+
+    public static void main(String[] args) {
+            
+    }
+}
