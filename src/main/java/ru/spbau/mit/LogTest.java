1f618af7948d7a7f6f9c6e44a7984444ea5f87ba
YorovSobir <sobir.yorov94@gmail.com>
2017-03-20 23:26:53 +0300

    log4j2 seminar

--- src/main/java/ru/spbau/mit/LogTest.java
+++ src/main/java/ru/spbau/mit/LogTest.java
@@ -1,13 +1,16 @@
 package ru.spbau.mit;
 
 import org.apache.logging.log4j.*;
-import java.lang.Double;
-import java.lang.Integer;
 
 public class LogTest {
-    private static final Logger LOGGER = LogManager.getLogger(LogTest.class);
+    private static final Logger LOGGER = LogManager.getLogger(LogTest.class.getName());
 
     public static void main(String[] args) {
-            
+        LOGGER.fatal("it's fatal");
+        LOGGER.error("it's error");
+        LOGGER.info("it's info");
+        LOGGER.warn("it's warn");
+        LOGGER.trace("it's trace");
+        LOGGER.debug("it's debug");
     }
 }
