7bf73de98af4b14c4785c45a6ab1583906909787
KruchDmitriy <kruch.dmitriy@gmail.com>
2017-04-15 00:02:52 +0300

    remove randomness

--- src/test/java/ru/spbau/mit/CollectionsTest.java
+++ src/test/java/ru/spbau/mit/CollectionsTest.java
@@ -1,18 +1,14 @@
 package ru.spbau.mit;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import static junitx.framework.Assert.assertEquals;
 
 public class CollectionsTest {
-    private static final int ARRAY_LENGTH = 10;
-    private static final int MAX_RAND_VALUE = 200;
-
     private static final Function1<Integer, Integer> SQR =
             new Function1<Integer, Integer>() {
                 @Override
@@ -71,39 +67,25 @@ public class CollectionsTest {
                 }
             };
 
-
-    private final Random random = new Random();
-    private ArrayList<Integer> sequence;
-
-    @Before
-    public void setUp() {
-        sequence = new ArrayList<>(ARRAY_LENGTH);
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            sequence.add(random.nextInt(MAX_RAND_VALUE));
-        }
-    }
-
-    private void setIdSequence() {
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            sequence.set(i, i);
-        }
-    }
+    private static final int ARRAY_LENGTH = 10;
+    private static final List<Integer> SEQUENCE =
+            Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
     @Test
     public void testMap() {
-        List<Integer> mappedSequence = Collections.map(sequence, SQR);
+        List<Integer> mappedSequence = Collections.map(SEQUENCE, SQR);
         for (int i = 0; i < ARRAY_LENGTH; i++) {
-            assertEquals((Integer) (sequence.get(i) * sequence.get(i)),
+            assertEquals((Integer) (SEQUENCE.get(i) * SEQUENCE.get(i)),
                     mappedSequence.get(i));
         }
     }
 
     @Test
     public void testFilter() {
-        List<Integer> filteredSeq = Collections.filter(sequence, LESS_THAN_ZERO);
+        List<Integer> filteredSeq = Collections.filter(SEQUENCE, LESS_THAN_ZERO);
 
         ArrayList<Integer> trueFilteredSeq = new ArrayList<>(ARRAY_LENGTH);
-        for (Integer element : sequence) {
+        for (Integer element : SEQUENCE) {
             if (element < 0) {
                 trueFilteredSeq.add(element);
             }
@@ -116,26 +98,24 @@ public class CollectionsTest {
 
     @Test
     public void testFilterAll() {
-        List<Integer> filteredSeq = Collections.filter(sequence,
+        List<Integer> filteredSeq = Collections.filter(SEQUENCE,
                 Predicate.ALWAYS_TRUE);
 
-        for (int i = 0; i < sequence.size(); i++) {
-            assertEquals(sequence.get(i), filteredSeq.get(i));
+        for (int i = 0; i < SEQUENCE.size(); i++) {
+            assertEquals(SEQUENCE.get(i), filteredSeq.get(i));
         }
 
-        filteredSeq = Collections.filter(sequence, Predicate.ALWAYS_FALSE);
+        filteredSeq = Collections.filter(SEQUENCE, Predicate.ALWAYS_FALSE);
         assertEquals(0, filteredSeq.size());
     }
 
     @Test
     public void testTakeWhile() {
-        setIdSequence();
-
         List<Integer> takeWhileSequence =
-                Collections.takeWhile(sequence, LESS_THAN_FIVE);
+                Collections.takeWhile(SEQUENCE, LESS_THAN_FIVE);
 
         ArrayList<Integer> trueTakeWhileSequence = new ArrayList<>(ARRAY_LENGTH);
-        for (Integer element : sequence) {
+        for (Integer element : SEQUENCE) {
             if (LESS_THAN_FIVE.apply(element)) {
                 break;
             }
@@ -147,25 +127,23 @@ public class CollectionsTest {
                     takeWhileSequence.get(i));
         }
 
-        takeWhileSequence = Collections.takeWhile(sequence,
+        takeWhileSequence = Collections.takeWhile(SEQUENCE,
                 Predicate.ALWAYS_TRUE);
-        for (int i = 0; i < sequence.size(); i++) {
-            assertEquals(sequence.get(i), takeWhileSequence.get(i));
+        for (int i = 0; i < SEQUENCE.size(); i++) {
+            assertEquals(SEQUENCE.get(i), takeWhileSequence.get(i));
         }
 
-        takeWhileSequence = Collections.takeWhile(sequence,
+        takeWhileSequence = Collections.takeWhile(SEQUENCE,
                 Predicate.ALWAYS_FALSE);
         assertEquals(0, takeWhileSequence.size());
     }
 
     @Test
     public void testTakeUnless() {
-        setIdSequence();
-
         List<Integer> takeUnlessSequence =
-                Collections.takeUnless(sequence, GREATER_THAN_FIVE);
+                Collections.takeUnless(SEQUENCE, GREATER_THAN_FIVE);
         ArrayList<Integer> trueTakeUnlessSequence = new ArrayList<>(ARRAY_LENGTH);
-        for (Integer element : sequence) {
+        for (Integer element : SEQUENCE) {
             if (GREATER_THAN_FIVE.apply(element)) {
                 break;
             }
@@ -177,44 +155,34 @@ public class CollectionsTest {
                     takeUnlessSequence.get(i));
         }
 
-        takeUnlessSequence = Collections.takeUnless(sequence, LESS_THAN_ARRAY_LENGTH);
+        takeUnlessSequence = Collections.takeUnless(SEQUENCE, LESS_THAN_ARRAY_LENGTH);
 
         assertEquals(0, takeUnlessSequence.size());
 
-        takeUnlessSequence = Collections.takeUnless(sequence, LESS_THAN_ZERO);
-        for (int i = 0; i < sequence.size(); i++) {
-            assertEquals(sequence.get(i), takeUnlessSequence.get(i));
+        takeUnlessSequence = Collections.takeUnless(SEQUENCE, LESS_THAN_ZERO);
+        for (int i = 0; i < SEQUENCE.size(); i++) {
+            assertEquals(SEQUENCE.get(i), takeUnlessSequence.get(i));
         }
     }
 
     @Test
     public void testFoldrSum() {
-        Integer sumValue = 0;
-        for (Integer element : sequence) {
-            sumValue += element;
-        }
-
-        Number value = Collections.foldr(sequence, SUM, 0);
-        assertEquals(sumValue.doubleValue(), value);
+        Number value = Collections.foldr(SEQUENCE, SUM, 0);
+        final Double fortyFive = 45.;
+        assertEquals(fortyFive, value);
     }
 
     @Test
     public void testFoldrSub() {
-        setIdSequence();
-
-        Integer value = Collections.foldr(sequence, SUB, 0);
+        Integer value = Collections.foldr(SEQUENCE, SUB, 0);
         final Integer minusFive = -5;
         assertEquals(minusFive, value);
     }
 
     @Test
     public void testFoldlSum() {
-        Integer sumValue = 0;
-        for (Integer element : sequence) {
-            sumValue += element;
-        }
-
-        Number value = Collections.foldl(sequence, SUM, 0);
-        assertEquals(sumValue.doubleValue(), value);
+        Number value = Collections.foldl(SEQUENCE, SUM, 0);
+        final Double fortyFive = 45.;
+        assertEquals(fortyFive, value);
     }
 }
