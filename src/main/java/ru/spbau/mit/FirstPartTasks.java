fb73aed5d09142ef65e055f3698ca1ddbd166ddd
sil <asml.silence@gmail.com>
2017-04-27 22:07:14 +0300

    Impl task

--- src/main/java/ru/spbau/mit/FirstPartTasks.java
+++ src/main/java/ru/spbau/mit/FirstPartTasks.java
@@ -1,73 +1,115 @@
 package ru.spbau.mit;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public final class FirstPartTasks {
+    static final int RATING_THRESHOLD = 95;
+
     private FirstPartTasks() {}
     // Список названий альбомов
     public static List<String> allNames(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return albums
+            .map(Album::getName)
+            .collect(Collectors.toList());
     }
 
     // Список названий альбомов, отсортированный лексикографически по названию
     public static List<String> allNamesSorted(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return albums
+            .map(Album::getName)
+            .sorted()
+            .collect(Collectors.toList());
     }
 
     // Список треков, отсортированный лексикографически по названию, включающий все треки альбомов из 'albums'
     public static List<String> allTracksSorted(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return albums
+            .flatMap(p -> p.getTracks().stream())
+            .map(Track::getName)
+            .sorted()
+            .collect(Collectors.toList());
     }
 
     // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
-    public static List<Album> sortedFavorites(Stream<Album> s) {
-        throw new UnsupportedOperationException();
+    public static List<Album> sortedFavorites(Stream<Album> albums) {
+        return albums
+            .filter(p -> p
+                .getTracks()
+                .stream()
+                .anyMatch(v -> v.getRating() > RATING_THRESHOLD))
+            .sorted(Comparator.comparing(Album::getName))
+            .collect(Collectors.toList());
     }
 
     // Сгруппировать альбомы по артистам
     public static Map<Artist, List<Album>> groupByArtist(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return albums.collect(Collectors.groupingBy(Album::getArtist));
     }
 
     // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'Album' использовать его имя)
     public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return albums.collect(
+            Collectors.groupingBy(Album::getArtist,
+                Collectors.mapping(Album::toString, Collectors.toList())));
     }
 
     // Число повторяющихся альбомов в потоке
     public static long countAlbumDuplicates(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return albums
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+            .values()
+            .stream()
+            .filter(p -> p > 1)
+            .count();
     }
 
     // Альбом в котором максимум рейтинга минимален
     // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
     public static Optional<Album> minMaxRating(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return albums.min(
+            Comparator.comparing(p -> p
+                .getTracks()
+                .stream()
+                .max(Comparator.comparing(Track::getRating))
+                .get()
+                .getRating())
+        );
     }
 
     // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
     public static List<Album> sortByAverageRating(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+        return albums
+            .sorted(Comparator.<Album>comparingDouble(p -> p
+                    .getTracks()
+                    .stream()
+                    .mapToLong(Track::getRating)
+                    .sum() / p.getTracks().size())
+                .reversed())
+            .collect(Collectors.toList());
     }
 
     // Произведение всех чисел потока по модулю 'modulo'
     // (все числа от 0 до 10000)
     public static int moduloProduction(IntStream stream, int modulo) {
-        throw new UnsupportedOperationException();
+        return stream.reduce(1, (u, v) -> (u * v) % modulo);
     }
 
     // Вернуть строку, состояющую из конкатенаций переданного массива, и окруженную строками "<", ">"
     // см. тесты
     public static String joinTo(String... strings) {
-        throw new UnsupportedOperationException();
+        return Arrays.stream(strings).collect(
+                Collectors.joining(", ", "<", ">")
+        );
     }
 
     // Вернуть поток из объектов класса 'clazz'
     public static <R> Stream<R> filterIsInstance(Stream<?> s, Class<R> clazz) {
-        throw new UnsupportedOperationException();
+        return s
+            .filter(p -> clazz.isAssignableFrom(p.getClass()))
+            .map(p -> (R) p);
     }
 }
--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,7 +1,14 @@
 package ru.spbau.mit;
 
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
@@ -9,7 +16,15 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream()
+            .flatMap(path -> {
+                try {
+                    return Files.lines(Paths.get(path));
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }).filter(p -> p.contains(sequence))
+            .collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +32,40 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        final Random random = new Random();
+        final double radius = 0.5;
+        final double centerX = 0.5;
+        final double centerY = 0.5;
+        final long shots = 100000;
+
+        final long hits = Stream
+            .generate(() -> new Point2D.Double(random.nextDouble(), random.nextDouble()))
+            .limit(shots)
+            .filter(pt -> pt.distance(centerX, centerY) <= radius)
+            .count();
+
+        return (double) hits / shots;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream()
+            .map(p -> new AbstractMap.SimpleEntry<String, Integer>(p.getKey(), p.getValue()
+                    .stream()
+                    .mapToInt(xs -> xs.length())
+                    .sum())
+            ).max(Comparator.comparing(p -> p.getValue()))
+            .get().getKey();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream()
+            .flatMap(p -> p.entrySet().stream())
+            .collect(Collectors.groupingBy(
+                Map.Entry<String, Integer>::getKey,
+                Collectors.summingInt(Map.Entry<String, Integer>::getValue)));
     }
 }
