49bd9cd7cd516e5149e9996d543ed99c10f86620
iasawseen <iasawseen@gmail.com>
2017-04-27 20:13:18 +0300

    sosin 05-streams updated

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,7 +1,19 @@
 package ru.spbau.mit;
 
+
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
@@ -9,7 +21,17 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream()
+                .flatMap(path -> {
+                    try {
+                        return Files.lines(Paths.get(path));
+                    }
+                    catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                })
+                .filter(str -> str.contains(sequence))
+                .collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +39,31 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        final long trials = 6666666;
+        final double radius = 0.5;
+
+        return Stream.generate(() -> Math.pow(Math.random() - radius, 2) +
+                Math.pow(Math.random() - radius, 2) <= Math.pow(radius, 2))
+                .limit(trials)
+                .filter(a -> a)
+                .count() / (double) trials;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(entry -> entry.getValue().stream().mapToLong(String::length).sum()))
+                .get().getKey();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream()
+                .flatMap(map -> map.entrySet().stream())
+                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)));
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -2,27 +2,70 @@ package ru.spbau.mit;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static ru.spbau.mit.SecondPartTasks.*;
+import static org.junit.Assert.*;
+import com.google.common.collect.ImmutableMap;
+
 
 public class SecondPartTasksTest {
 
     @Test
     public void testFindQuotes() {
-        fail();
+        List<String> paths = Arrays.asList("src/test/resources/first.txt",
+                "src/test/resources/second.txt",
+                "src/test/resources/third.txt",
+                "src/test/resources/fourth.txt");
+
+        String quote = "Dark";
+        List<String> expected = Arrays.asList("In grim Dark of the future there is only war.",
+                "Without the Dark, there can be no Light,");
+        List<String> actual = findQuotes(paths, quote);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        assertEquals(Math.PI / 4.0, piDividedBy4(), 0.001);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        String composer1 = "comp1";
+        String composer2 = "comp2";
+        List<String> compositions1 = Arrays.asList("I shall know no fear", "Behold your king");
+        List<String> compositions2 = Arrays.asList("To infinity and beyond", "blablaland");
+
+        Map<String, List<String>> map = new TreeMap<>();
+        map.put(composer1, compositions1);
+        map.put(composer2, compositions2);
+
+        assertEquals(composer1, findPrinter(map));
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        String item1 = "sock";
+        String item2 = "candy";
+        String item3 = "shroom";
+        String item4 = "bra";
+
+        List<Map<String, Integer>> actual = Arrays.asList(
+                ImmutableMap.of(item1, 3, item2, 4, item3, 2),
+                ImmutableMap.of(item1, 2, item3, 6),
+                ImmutableMap.of(item2, 3, item4, 3));
+
+        assertEquals(
+                ImmutableMap.of(
+                        item1, 5,
+                        item2, 7,
+                        item3, 8,
+                        item4, 3),
+                calculateGlobalOrder(actual));
     }
 }
