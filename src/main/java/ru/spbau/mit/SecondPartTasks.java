64b0206657ead59e069a212335e26c3be8a8c138
KruchDmitriy <kruch.dmitriy@gmail.com>
2017-04-27 19:37:21 +0300

    05-streams

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,7 +1,8 @@
 package ru.spbau.mit;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
@@ -9,7 +10,9 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream()
+            .filter(s -> s.contains(sequence))
+            .collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +20,48 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        Random rng = new Random();
+        final double radius = 0.5;
+        final double center = 0.5;
+        final int numberOfShoots = 1_000_000;
+        return Stream
+            .iterate(0, n -> n + 1)
+            .mapToDouble(i -> {
+                Double x = rng.nextDouble();
+                Double y = rng.nextDouble();
+                return (x - center) * (x - center)
+                        + (y - center) * (y - center) < radius * radius
+                        ? 1.0 : 0.0;
+            })
+            .limit(numberOfShoots)
+            .average()
+            .orElse(0.);
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream()
+                .max(Comparator.comparingInt(
+                        stringListEntry -> stringListEntry.getValue().stream()
+                        .mapToInt(String::length)
+                        .sum()
+                ))
+                .map(Map.Entry::getKey)
+                .orElse("");
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream()
+                .flatMap(map -> map.entrySet().stream())
+                .collect(
+                        Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (e1, e2) -> e1 + e2
+                                )
+                );
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -1,28 +1,125 @@
 package ru.spbau.mit;
 
+
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static ru.spbau.mit.SecondPartTasks.*;
 
 public class SecondPartTasksTest {
 
     @Test
     public void testFindQuotes() {
-        fail();
+        final List<String> emptyList = new ArrayList<>();
+        assertEquals(emptyList, findQuotes(emptyList, "lorem"));
+
+        final List<String> test = Arrays.asList("ipsum ipsum", "lorem ipsum",
+                "lorem", "tell me why", "how not to sleep and stay strong",
+                "don't worry, be happy, lorem", "save my lorem, tristan", "");
+
+        assertEquals(emptyList, findQuotes(test, "nonExistingWord"));
+
+        assertEquals(Arrays.asList("lorem ipsum", "lorem",
+                "don't worry, be happy, lorem", "save my lorem, tristan"),
+                findQuotes(test, "lorem"));
+
+        assertEquals(test, findQuotes(test, ""));
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        final double piDiv4 = Math.PI / 4.;
+        final double eps = 1e-2;
+
+        assertEquals(piDividedBy4(), piDiv4, eps);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        final HashMap<String, List<String>> compositions = new HashMap<String, List<String>>() {{
+                put("Lermontov", Arrays.asList(
+                        "Tell me my uncle",
+                        "it's not for nothing,",
+                        "Moscow",
+                        "burnt by a fire to the Frenchman is given?"));
+                put("Esenin", Arrays.asList(
+                        "Oh",
+                        "you are my native Russia",
+                        "huts in garments of the image",
+                        "do not see the end and edges",
+                        "only blue sucks eyes"));
+                put("Hocku", Arrays.asList("The cat is wet",
+                        "and the dog is wet.",
+                        "It's a not a happy dog",
+                        "it's a not a happy cat"));
+
+                put("Father", Collections.singletonList("I'm your father"));
+                put("SOS", Collections.emptyList());
+            }};
+
+        assertEquals("Esenin", findPrinter(compositions));
+
+        final HashMap<String, List<String>> smallCompositions = new HashMap<String, List<String>>() {{
+            put("aba", Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h"));
+            put("bab", Collections.singletonList("abcdefg"));
+            put("bb", Collections.emptyList());
+        }};
+
+        assertEquals("aba", findPrinter(smallCompositions));
+
+        final HashMap<String, List<String>> longCompositions = new HashMap<String, List<String>>() {{
+            put("aba", Arrays.asList("a", "b", "c", "d", "e", "f"));
+            put("bab", Collections.singletonList("abcdefghijk"));
+            put("bb", Collections.emptyList());
+        }};
+
+        assertEquals("bab", findPrinter(longCompositions));
+
+        assertEquals("", findPrinter(new HashMap<>()));
     }
 
+    // CHECKSTYLE:OFF
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        HashMap<String, Integer> order1 = new HashMap<String, Integer>() {{
+            put("1", 1);
+            put("2", 2);
+        }};
+
+        HashMap<String, Integer> order2 = new HashMap<String, Integer>() {{
+            put("1", 2);
+            put("2", 4);
+        }};
+
+        HashMap<String, Integer> order3 = new HashMap<String, Integer>() {{
+            put("3", 3);
+            put("5", 5);
+        }};
+
+        HashMap<String, Integer> order4 = new HashMap<String, Integer>() {{
+            put("5", 3);
+            put("12", 0);
+        }};
+
+        List<Map<String, Integer>> orders = Arrays.asList(
+                order1, order2, order3, order4
+        );
+
+        HashMap<String, Integer> resultOrder = new HashMap<String, Integer>() {{
+            put("1", 3);
+            put("2", 6);
+            put("3", 3);
+            put("5", 8);
+            put("12", 0);
+        }};
+
+        assertEquals(resultOrder, calculateGlobalOrder(orders));
+
+
+        final List<Map<String, Integer>> emptyList = new ArrayList<>();
+        assertEquals(Collections.EMPTY_MAP, calculateGlobalOrder(emptyList));
     }
+    // CHECKSTYLE:ON
 }
