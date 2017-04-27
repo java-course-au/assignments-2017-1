eead39e7387b0e7e2b4f44cf626c4028865c12ff
bugogish <bugogish@gmail.com>
2017-04-27 12:54:08 +0300

    hw implemented

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,7 +1,16 @@
 package ru.spbau.mit;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
 public final class SecondPartTasks {
 
@@ -9,7 +18,13 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream().flatMap(p -> {
+            try {
+                return Files.lines(Paths.get(p));
+            } catch (IOException e) {
+                return null;
+            }
+        }).filter(s -> s.contains(sequence)).collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +32,31 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        Random random = new Random();
+        final int numExperiments = 1000000;
+        double x0 = 0.5;
+        double y0 = 0.5;
+
+        Stream<Double> experiments = Stream.generate(() -> sqrt(pow(random.nextDouble() - x0, 2) + pow(random.nextDouble() - y0, 2)));
+        return (double) experiments.limit(numExperiments).filter(s -> s <= 1.0 / 2.0).count() / numExperiments;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream().collect(Collectors.toMap(
+                Map.Entry::getKey,
+                e -> e.getValue().stream().mapToInt(String::length).sum()
+        )).entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream().flatMap(m -> m.entrySet().stream()).collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                (g1, g2) -> g1 + g2
+        ));
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -2,27 +2,89 @@ package ru.spbau.mit;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.io.File;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SecondPartTasksTest {
 
     @Test
     public void testFindQuotes() {
-        fail();
+        ClassLoader classLoader = getClass().getClassLoader();
+        File testFile1 = new File(classLoader.getResource("quotes1").getFile());
+        File testFile2 = new File(classLoader.getResource("quotes2").getFile());
+        List<String> paths = new ArrayList<>();
+        paths.add(testFile1.getAbsolutePath());
+        paths.add(testFile2.getAbsolutePath());
+
+        assertEquals(
+                Arrays.asList("hello",
+                        "hello world",
+                        "that is hell down below",
+                        "testhelltest",
+                        "testheltest testhelltest testhel ltest",
+                        "that's new hello world",
+                        "ha hello you",
+                        "that is a test to find hell string"),
+                SecondPartTasks.findQuotes(paths, "hell"));
+
+        assertEquals(
+                Arrays.asList("hello",
+                        "hello world",
+                        "that's new hello world",
+                        "ha hello you"),
+                SecondPartTasks.findQuotes(paths, "hello"));
+
+        assertEquals(
+                new ArrayList<String>(),
+                SecondPartTasks.findQuotes(paths, "NO_SUCH_WORD"));
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        final double probability = SecondPartTasks.piDividedBy4();
+        final double precision = 0.01;
+        assertTrue(Math.abs(probability - Math.PI / 4) < precision);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        String[] texts = {"abc", "abcd", "abcde"};
+        Map<String, List<String>> compositions = new HashMap<>();
+        compositions.put("Author1",
+                Arrays.asList(texts[1],
+                        texts[1],
+                        texts[2]));
+        compositions.put("Author2",
+                Arrays.asList(texts[2],
+                        texts[2],
+                        texts[2]));
+        compositions.put("Author3",
+                Arrays.asList(texts[1],
+                        texts[0],
+                        texts[1]));
+
+        assertEquals("Author2", SecondPartTasks.findPrinter(compositions));
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        List<Map<String, Integer>> orders = new ArrayList<>();
+        orders.add(new HashMap<>());
+        orders.add(new HashMap<>());
+        orders.add(new HashMap<>());
+        orders.get(0).put("Eggs", 1000);
+        orders.get(0).put("Milk", 10);
+        orders.get(1).put("Eggs", 100);
+        orders.get(1).put("Milk", 100);
+        orders.get(2).put("Eggs", 10);
+        orders.get(2).put("Milk", 1001);
+
+        Map<String, Integer> globalOrder = SecondPartTasks.calculateGlobalOrder(orders);
+
+        assertEquals(new Integer(1110), globalOrder.get("Eggs"));
+        assertEquals(new Integer(1111), globalOrder.get("Milk"));
     }
 }
