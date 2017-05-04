4516cc934924e4e790a94b53e6f1b0debe872081
simiyutin <simiyutin.boris@yandex.ru>
2017-05-05 00:04:36 +0300

    files were forgotten

--- src/test/java/ru/spbau/mit/MemoryLeakLimit.java
+++ src/test/java/ru/spbau/mit/MemoryLeakLimit.java
@@ -1,7 +1,39 @@
 package ru.spbau.mit;
 
-/**
- * Created by boris on 04.05.17.
- */
-public class MemoryLeakLimit {
+import org.junit.AssumptionViolatedException;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+
+public class MemoryLeakLimit implements TestRule {
+
+    private long limit = 0;
+
+    void limit(long mb) {
+        this.limit = 1024 * 1024 * mb;
+    }
+
+    @Override
+    public Statement apply(final Statement statement, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+
+                Runtime runtime = Runtime.getRuntime();
+
+                runtime.gc();
+                long freeBefore = runtime.freeMemory();
+
+                statement.evaluate();
+
+                runtime.gc();
+                long freeAfter = runtime.freeMemory();
+
+                if (freeBefore - freeAfter > limit) {
+                    throw new AssumptionViolatedException("memory leak limit exceeded");
+                }
+            }
+        };
+    }
 }
--- src/test/java/ru/spbau/mit/MemoryLeakLimitTest.java
+++ src/test/java/ru/spbau/mit/MemoryLeakLimitTest.java
@@ -1,7 +1,25 @@
 package ru.spbau.mit;
 
-/**
- * Created by boris on 04.05.17.
- */
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemoryLeakLimitTest {
+
+    private static List<Integer> list = new ArrayList<>();
+
+    @Rule
+    public MemoryLeakLimit leakLimitRule = new MemoryLeakLimit();
+
+    @Test
+    public void testMemoryLeakLimitFail() {
+        leakLimitRule.limit(2);
+        final int numberOfInts = 1024 * 1024;
+        for (int i = 0; i < numberOfInts; i++) {
+            list.add(1000);
+        }
+    }
 }
