7720abc8559892f1d66ea49b317e9861d652fd51
bugogish <bugogish@gmail.com>
2017-05-18 17:42:31 +0300

    more tests added

--- src/test/java/ru/spbau/mit/MemoryLeakTest.java
+++ src/test/java/ru/spbau/mit/MemoryLeakTest.java
@@ -14,12 +14,46 @@ public class MemoryLeakTest {
 
 
     @Test
-    public void memTest() {
+    public void memTestFail() throws Exception {
         final int bigNumber = 10000000;
         leak.limit(1);
 
         for (Integer i = 0; i < bigNumber; i++) {
             vec.add(i);
         }
+    }
+
+    @Test
+    public void memTestPass() throws Exception {
+        final int smallNumber = 10;
+        leak.limit(1);
+
+        for (Integer i = 0; i < smallNumber; i++) {
+            vec.add(i);
+        }
+    }
+
+    @Test(expected = Exception.class)
+    public void memTestExpectedExceptionMemFail() throws Exception {
+        final int bigNumber = 10000000;
+        leak.limit(1);
+
+        for (Integer i = 0; i < bigNumber; i++) {
+            vec.add(i);
+        }
+
+        throw new Exception();
+    }
+
+    @Test(expected = Exception.class)
+    public void memTestExpectedExceptionButNotMemFail() throws Exception {
+        final int smallNumber = 10;
+        leak.limit(1);
+
+        for (Integer i = 0; i < smallNumber; i++) {
+            vec.add(i);
+        }
+
+        throw new Exception("Test");
     }
 }
