890c968b8a6b029e0c64791cf358d1d0c4fb5f0b
KruchDmitriy <kruch.dmitriy@gmail.com>
2017-05-20 00:11:52 +0300

    we need more memory!

--- src/test/java/ru/spbau/mit/MemoryLeakLimitTest.java
+++ src/test/java/ru/spbau/mit/MemoryLeakLimitTest.java
@@ -1,17 +1,28 @@
 package ru.spbau.mit;
 
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.Random;
+
 public class MemoryLeakLimitTest {
     private static int[] memory;
-    private static final int TEST_MEMORY_SIZE = 100000;
+    private static final int TEST_MEMORY_SIZE = 1_000_000;
+    private static final Random RNG = new Random();
 
     @Rule
     public final MemoryLeakLimit memoryLeakLimit = new MemoryLeakLimit();
 
-    // CHECKSTYLE:OFF
+    @After
+    public void after() {
+        if (memory != null) {
+            memory[RNG.nextInt(TEST_MEMORY_SIZE)] =
+                    memory[RNG.nextInt(TEST_MEMORY_SIZE)];
+        }
+    }
+
     @Test
     public void testHugeMemoryAllocation() {
         memoryLeakLimit.limit(1);
@@ -24,15 +35,15 @@ public class MemoryLeakLimitTest {
         memory = new int[TEST_MEMORY_SIZE];
         throw new Exception("Some exception");
     }
-    // CHECKSTYLE:ON
 
     @Test(expected = Exception.class)
     public void testException() throws Exception {
+        memoryLeakLimit.limit(0);
         throw new Exception("Some exception");
     }
 
     @Test
     public void testNoMemoryAllocation() {
-        memoryLeakLimit.limit(0);
+        memoryLeakLimit.limit(1);
     }
 }
