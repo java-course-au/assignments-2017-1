3441187b725bdd11000cd4c7551dcfc79145e2b6
sil <asml.silence@gmail.com>
2017-05-19 10:36:37 +0300

    Add simple impl

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,10 +1,60 @@
 package ru.spbau.mit;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public final class Injector {
-    private Injector() {
+    private Class root;
+    private List<Class> imps;
+
+    private Injector(String rootn, List<String> impsn) throws ImplementationNotFoundException {
+        ClassLoader loader = this.getClass().getClassLoader();
+        try {
+            root = loader.loadClass(rootn);
+            imps = new ArrayList<>();
+            for (String cl : impsn) {
+                imps.add(loader.loadClass(cl));
+            }
+        } catch (ClassNotFoundException e) {
+            throw new ImplementationNotFoundException();
+        }
+    }
+
+    private Class find(Class trg, Class dep) throws Exception {
+        List<Class> cls = imps.stream().filter(x -> trg.isAssignableFrom(x)).collect(Collectors.toList());
+        if (cls.size() == 0) {
+            throw new ImplementationNotFoundException();
+        } else if (cls.size() != 1) {
+            throw new AmbiguousImplementationException();
+        } else if (cls.get(0) == dep) {
+            throw new InjectionCycleException();
+        } else {
+            return cls.get(0);
+        }
+    }
+
+    private Object subs(Class trg, Class dep) throws Exception {
+        Constructor ctr = trg.getConstructors()[0];
+        Class[] paramst = ctr.getParameterTypes();
+        List<Object> params = new ArrayList<>();
+
+        for (Class pt : paramst) {
+            Class cl = find(pt, dep);
+            params.add(subs(cl, dep));
+        }
+
+        try {
+            return ctr.newInstance(params.toArray());
+        } catch (Exception e) {
+            throw new ImplementationNotFoundException();
+        }
+    };
+
+    private Object get() throws Exception {
+        return subs(root, root);
     }
 
     /**
@@ -12,6 +62,7 @@ public final class Injector {
      * `implementationClassNames` for concrete dependencies.
      */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        Injector injector = new Injector(rootClassName, implementationClassNames);
+        return injector.get();
     }
 }
