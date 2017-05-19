e036551fb9eb34f8fab587549cb48a2c406af883
bugogish <bugogish@gmail.com>
2017-05-19 11:24:34 +0300

    initial commit

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,6 +1,8 @@
 package ru.spbau.mit;
 
-import java.util.List;
+import java.lang.reflect.Constructor;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public final class Injector {
@@ -12,6 +14,85 @@ public final class Injector {
      * `implementationClassNames` for concrete dependencies.
      */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        ClassLoader loader = Injector.class.getClassLoader();
+        Map<String, Object> initialized = new HashMap<>();
+        Set<String> roots = new HashSet<>();
+
+        List<Class<?>> implementations = implementationClassNames.stream().map(s -> {
+            try {
+                return loader.loadClass(s);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).collect(Collectors.toList());
+
+        Class root = loader.loadClass(rootClassName);
+
+        Constructor<?> rootConstructor = root.getConstructors()[0];
+        Class<?>[] dependencies = rootConstructor.getParameterTypes();
+        List<Object> parameters = new ArrayList<>();
+        for (Class<?> dependency : dependencies) {
+            List<String> realisations = implementations.stream().
+                    filter(dependency::isAssignableFrom).
+                    map(Class::getCanonicalName).
+                    collect(Collectors.toList());
+
+            if (realisations.isEmpty()) {
+                throw new ImplementationNotFoundException();
+            }
+            if (realisations.size() > 1) {
+                throw new AmbiguousImplementationException();
+            }
+            initializeChildren(initialized, realisations.get(0), implementationClassNames, roots);
+            parameters.add(initialized.get(realisations.get(0)));
+        }
+
+        return rootConstructor.newInstance(parameters.toArray());
+    }
+
+    private static void initializeChildren(Map<String, Object> initialized,
+                                           String rootClassName,
+                                           List<String> implementationClassNames,
+                                           Set<String> roots) throws Exception {
+
+        ClassLoader loader = Injector.class.getClassLoader();
+        roots.add(rootClassName);
+        List<Class<?>> implementations = implementationClassNames.stream().map(s -> {
+            try {
+                return loader.loadClass(s);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).collect(Collectors.toList());
+
+        Class root = loader.loadClass(rootClassName);
+
+        Constructor<?> rootConstructor = root.getConstructors()[0];
+        Class<?>[] dependencies = rootConstructor.getParameterTypes();
+        List<Object> parameters = new ArrayList<>();
+        for (Class<?> dependency : dependencies) {
+            List<String> realisations = implementations.stream().
+                    filter(dependency::isAssignableFrom).
+                    map(Class::getCanonicalName).
+                    collect(Collectors.toList());
+
+            if (realisations.isEmpty()) {
+                throw new ImplementationNotFoundException();
+            }
+            if (realisations.size() > 1) {
+                throw new AmbiguousImplementationException();
+            }
+            if (roots.contains(realisations.get(0))) {
+                throw new InjectionCycleException();
+            }
+
+            initializeChildren(initialized, realisations.get(0), implementationClassNames, roots);
+            parameters.add(initialized.get(realisations.get(0)));
+            roots.remove(realisations.get(0));
+        }
+
+        initialized.putIfAbsent(rootClassName, rootConstructor.newInstance(parameters.toArray()));
     }
 }
